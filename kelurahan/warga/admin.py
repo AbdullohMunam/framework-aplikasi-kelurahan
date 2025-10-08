129b0a58e8ad072de320375764bfa61a5756bd06
ABDULLOH MUNAM <49928894+AbdullohMunam@users.noreply.github.com>
2025-10-08 10:34:14 +0700

    first commit

---
+++ kelurahan/warga/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Warga, Pengaduan
+
+
+@admin.register(Warga)
+class WargaAdmin(admin.ModelAdmin):
+	list_display = ('nik', 'nama_lengkap', 'no_telepon', 'tanggal_registrasi')
+	search_fields = ('nik', 'nama_lengkap')
+
+
+@admin.register(Pengaduan)
+class PengaduanAdmin(admin.ModelAdmin):
+	list_display = ('judul', 'pelapor', 'status', 'tanggal_lapor')
+	list_filter = ('status', 'tanggal_lapor')
+	search_fields = ('judul', 'deskripsi', 'pelapor__nama_lengkap')
+
