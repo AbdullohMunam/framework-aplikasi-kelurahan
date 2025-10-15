06cd06a120d982ec6224289d373f49c7f3c14cd4
ABDULLOH MUNAM <49928894+AbdullohMunam@users.noreply.github.com>
2025-10-15 03:33:48 +0000

    Commit pertemuan 3 Praktikum

--- kelurahan/warga/views.py
+++ kelurahan/warga/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 from .models import Warga, Pengaduan
-
+from .forms import WargaForm
 
 class WargaListView(ListView):
     model = Warga
 
-
 class WargaDetailView(DetailView):
     model = Warga
-
 
 class PengaduanListView(ListView):
     model = Pengaduan
     template_name = 'warga/pengaduan_list.html'
+
+class WargaCreateView(CreateView):
+    model = Warga
+    form_class = WargaForm
+    template_name = 'warga/warga_form.html'
+    success_url = reverse_lazy('warga-list')
