915a43eaa61085ebca4a520b5d599ce16a25e016
ABDULLOH MUNAM <49928894+AbdullohMunam@users.noreply.github.com>
2025-10-15 03:51:16 +0000

    Commit Tugas chalenge Pertemuan 2

--- kelurahan/warga/forms.py
+++ kelurahan/warga/forms.py
@@ -1,9 +1,13 @@
 # warga/forms.py
 from django import forms
-from .models import Warga
+from .models import Warga, Pengaduan
 
 class WargaForm(forms.ModelForm):
     class Meta:
         model = Warga
-        # Tentukan field mana saja dari model yang ingin ditampilkan di form
         fields = ['nik', 'nama_lengkap', 'alamat', 'no_telepon']
+
+class PengaduanForm(forms.ModelForm):
+    class Meta:
+        model = Pengaduan
+        fields = ['pelapor', 'judul', 'deskripsi']
--- kelurahan/warga/views.py
+++ kelurahan/warga/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
 from .models import Warga, Pengaduan
-from .forms import WargaForm
+from .forms import WargaForm, PengaduanForm
 
 class WargaListView(ListView):
     model = Warga
@@ -19,3 +19,10 @@ class WargaCreateView(CreateView):
     form_class = WargaForm
     template_name = 'warga/warga_form.html'
     success_url = reverse_lazy('warga-list')
+
+
+class PengaduanCreateView(CreateView):
+    model = Pengaduan
+    form_class = PengaduanForm
+    template_name = 'warga/pengaduan_form.html'
+    success_url = reverse_lazy('pengaduan-list')
